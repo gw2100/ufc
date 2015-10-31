3f476a71a9abedc71c7d292abdbb5ce49be22358
george wang <george.wang@emory.edu>
2015-10-30 22:20:45 -0400

    before adding rest-java stuff

--- src/main/java/us/galleryw/ufc/domain/User.java
+++ src/main/java/us/galleryw/ufc/domain/User.java
@@ -1,95 +1,65 @@
-package us.galleryw.ufc.domain;
+package us.galleryw.ufc.backend;
 
-public final class User {
-    private String role;
-    private String firstName;
-    private String lastName;
-    private String title;
-    private boolean male;
-    private String email;
-    private String location;
-    private String phone;
-    private Integer newsletterSubscription;
-    private String website;
-    private String bio;
+import java.io.Serializable;
+import java.util.Date;
 
-    public String getEmail() {
-        return email;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+
+/**
+ * A simple DTO for the address book example.
+ *
+ * Serializable and cloneable Java Object that are typically persisted
+ * in the database and can also be easily converted to different formats like JSON.
+ */
+// Backend DTO class. This is just a typical Java backend implementation
+// class and nothing Vaadin specific.
+@Entity
+public class User implements Serializable, Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName = "";
+    private String lastName = "";
+    private String phone = "";
+    @Column(unique=true)
+    private String email = "";
+    private Date registrationDate;
+    private String password="";
+    private String roles;
+    private Date lastUpdateDate;
+
+    private String title="";
+    private String gender="";
+    private String location="";
+    private String website="";
+    private String bio="";
+    
+    @Column(length = 500000)
+    private byte[] thumbnail;   
+    @Version
+    private Integer version;
+    public Long getId() {
+        return id;
     }
 
-    public void setEmail(final String email) {
-        this.email = email;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(final String location) {
-        this.location = location;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(final String phone) {
-        this.phone = phone;
-    }
-
-    public Integer getNewsletterSubscription() {
-        return newsletterSubscription;
-    }
-
-    public void setNewsletterSubscription(final Integer newsletterSubscription) {
-        this.newsletterSubscription = newsletterSubscription;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(final String website) {
-        this.website = website;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(final String bio) {
-        this.bio = bio;
-    }
-
-    public boolean isMale() {
-        return male;
-    }
-
-    public void setMale(final boolean male) {
-        this.male = male;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(final String title) {
-        this.title = title;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(final String role) {
-        this.role = role;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(final String firstName) {
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
@@ -97,8 +67,162 @@ public final class User {
         return lastName;
     }
 
-    public void setLastName(final String lastName) {
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public User clone() throws CloneNotSupportedException {
+        try {
+            return (User) BeanUtils.cloneBean(this);
+        } catch (Exception ex) {
+            throw new CloneNotSupportedException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", firstName=" + firstName
+                + ", lastName=" + lastName + ", phone=" + phone + ", email="
+                + email + ", registrationDate=" + registrationDate + '}';
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+ 
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+   public String getName(){
+       return firstName+" "+lastName;
+   }
+
+public String getTitle() {
+    return title;
+}
+
+public void setTitle(String title) {
+    this.title = title;
+}
+
+public String getGender() {
+    return gender;
+}
+
+public void setGender(String gender) {
+    this.gender = gender;
+}
+
+public String getLocation() {
+    return location;
+}
+
+public void setLocation(String location) {
+    this.location = location;
+}
+
+public String getWebsite() {
+    return website;
+}
+
+public void setWebsite(String website) {
+    this.website = website;
+}
+
+public String getBio() {
+    return bio;
+}
+
+public void setBio(String bio) {
+    this.bio = bio;
+}
+
+public byte[] getThumbnail() {
+    return thumbnail;
+}
+
+public void setThumbnail(byte[] thumbnail) {
+    this.thumbnail = thumbnail;
+}
+public boolean isAdmin(){
+    return getRoles()!=null&&getRoles().contains("admin");
+}
+
+@Override
+public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((email == null) ? 0 : email.hashCode());
+    return result;
+}
+
+@Override
+public boolean equals(Object obj) {
+    if (this == obj)
+        return true;
+    if (obj == null)
+        return false;
+    if (getClass() != obj.getClass())
+        return false;
+    User other = (User) obj;
+    if (email == null) {
+        if (other.email != null)
+            return false;
+    } else if (!email.equals(other.email))
+        return false;
+    return true;
+}
+
+public Integer getVersion() {
+    return version;
+}
+
+public void setVersion(Integer version) {
+    this.version = version;
+}
+
 
 }
